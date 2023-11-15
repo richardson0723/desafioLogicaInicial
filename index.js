df262a66c25cfb31843757d5470cc3372a269ad9
richardson serena <92805831+richardson0723@users.noreply.github.com>
2023-11-15 15:02:29 -0300

    index.js

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 let personagem = "batman";
 let XP = 15000;
 
-switch (XP) {
-    case XP <= 1001, XP = "ferro":
-        console.log("O " + personagem + " esta no nivel " + XP)
-        break;
-    case XP <= 2001, XP = "bronze":
-        console.log("O " + personagem + " esta no nivel " + XP)
-        break;
-    case XP <= 5001, XP = "prata":
-        console.log("O " + personagem + " esta no nivel " + XP)
-        break;
-    case XP <= 7000, XP = "ouro":
-        console.log("O " + personagem + " esta no nivel " + XP)
-        break;
-    case XP <= 8000, XP = "platina":
-        console.log("O " + personagem + " esta no nivel " + XP)
-        break;
-    case XP <= 9000, XP = "Ascendente":
-        console.log("O " + personagem + " esta no nivel " + XP)
-        break;
-    case XP < 10000, XP = "Imortal":
-        console.log("O " + personagem + " esta no nivel " + XP)
-        break;
-    case XP >= 10001, XP = "Radiante":
-        console.log("O " + personagem + " esta no nivel " + XP)
-        break;
+
+let nivel;
+
+if (XP < 1000) {
+     = "Ferro";
+} else if (XP >= 1001 && pontosXP <= 2000) {
+    nive = "Bronze";
+} else if (XP >= 2001 && pontosXP <= 5000) {
+    nivel = "Prata";
+} else if (XP >= 5001 && pontosXP <= 7000) {
+    nivel = "Ouro";
+} else if (XP >= 7001 && pontosXP <= 8000) {
+    nivel= "Platina";
+} else if (XP >= 8001 && pontosXP <= 9000) {
+    nivel = "Ascendente";
+} else if (XP >= 9001 && pontosXP <= 10000) {
+    nivel = "Imortal";
+} else {
+   XP = "Radiante";
+}
 }
 console.log("O " + personagem + " esta no nivel " + XP)
