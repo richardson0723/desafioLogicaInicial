64dfadc43410ee9d0389d2bad5a9b32d4ab49522
richardson serena <92805831+richardson0723@users.noreply.github.com>
2023-11-15 14:52:15 -0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("ola mundo!")
+let personagem = "batman";
+let XP = 900;
+
+switch (XP) {
+    case XP <= 1001, XP = "ferro":
+        console.log("O " + personagem + " esta no nivel " + XP)
+        break;
+    case XP <= 2001, XP = "bronze":
+        console.log("O " + personagem + " esta no nivel " + XP)
+        break;
+    case XP <= 5001, XP = "prata":
+        console.log("O " + personagem + " esta no nivel " + XP)
+        break;
+    case XP <= 7000, XP = "ouro":
+        console.log("O " + personagem + " esta no nivel " + XP)
+        break;
+    case XP <= 8000, XP = "platina":
+        console.log("O " + personagem + " esta no nivel " + XP)
+        break;
+    case XP <= 9000, XP = "Ascendente":
+        console.log("O " + personagem + " esta no nivel " + XP)
+        break;
+    case XP < 10000, XP = "Imortal":
+        console.log("O " + personagem + " esta no nivel " + XP)
+        break;
+    case XP >= 10001, XP = "Radiante":
+        console.log("O " + personagem + " esta no nivel " + XP)
+        break;
+}
+console.log("O " + personagem + " esta no nivel " + XP)
