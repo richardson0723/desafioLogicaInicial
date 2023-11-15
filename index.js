86bcff98bd04e8f7c2a1c6f60fccbed9a2e1aba8
richardson serena <92805831+richardson0723@users.noreply.github.com>
2023-11-15 15:49:59 -0300

    index.js

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 let personagem = "batman";
 let XP = 15000;
 
-
-let nivel;
-
 if (XP < 1000) {
      = "Ferro";
-} else if (XP >= 1001 && pontosXP <= 2000) {
-    nive = "Bronze";
-} else if (XP >= 2001 && pontosXP <= 5000) {
+} else if (XP >= 1001 && XP <= 2000) {
+    nivel = "Bronze";
+} else if (XP >= 2001 && XP <= 5000) {
     nivel = "Prata";
-} else if (XP >= 5001 && pontosXP <= 7000) {
+} else if (XP >= 5001 && XP <= 7000) {
     nivel = "Ouro";
-} else if (XP >= 7001 && pontosXP <= 8000) {
+} else if (XP >= 7001 && XP <= 8000) {
     nivel= "Platina";
-} else if (XP >= 8001 && pontosXP <= 9000) {
+} else if (XP >= 8001 && XP <= 9000) {
     nivel = "Ascendente";
-} else if (XP >= 9001 && pontosXP <= 10000) {
+} else if (XP >= 9001 && XP <= 10000) {
     nivel = "Imortal";
 } else {
    XP = "Radiante";
